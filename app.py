eb839bda3ed551bac07be7030efd97e363143973
Ermin Jamaković <73228834+ejamakovic@users.noreply.github.com>
2023-09-12 18:28:48 +0200

    Dodana ruta za brisanje chata.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from llama_cpp import Llama
 import re
 
+chat = "" 
 
 print("Loading model...")
 llm = Llama(model_path="./models/llama-2-7b-chat.bin", verbose=True)
@@ -15,15 +16,17 @@ def index():
 
 @app.route("/pitanje", methods=["POST"])
 def pitanje():
+    global chat
     pitanje = request.form.get("pitanje")
-
     # Za što duži odgovor staviti max_tokens=0
-    odgovor = llm("Q: " + pitanje  + " A: ", max_tokens=100, stop=["Question:", "Q"], echo=True)
+    odgovor = llm(chat + "Q: " + pitanje + " A: ", max_tokens=100, stop=["Q"], echo=True)
     odgovor = re.split("A:", odgovor["choices"][0]["text"])[1]
+    chat = chat + "Question: " + pitanje + "Answer: " + odgovor
     return odgovor
 
 @app.route("/model", methods=["POST"])
 def model():
+    global llm
     model = request.form.get("model")
     print("Loading model...")
     if model=="7B":
@@ -33,6 +36,12 @@ def model():
     print("Model changed.")
     return "Model uspješno promjenjen"
 
+
+@app.route("/izbrisiChat", methods= ["POST"])
+def izbrisi():
+    global chat
+    chat = ""
+    return "Izbrisan prethondi chat"
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port = 8080, debug = True)

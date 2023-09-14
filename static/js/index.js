78d5ca276ce99185d1d98a80372ed68dd46681d4
Ermin Jamaković <73228834+ejamakovic@users.noreply.github.com>
2023-09-14 16:27:17 +0200

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -2,31 +2,41 @@ var pitanje = "";
 
 function posaljiPitanje(){
     pitanje = document.getElementById('pitanje').value;
-    console.log(pitanje);
     var chat = document.getElementById("chat");
-    chat.innerHTML = chat.innerHTML + '<div class="user1"><img class="korisnik"><p class="message">' + pitanje + '</p></div>';
+    chat.innerHTML = chat.innerHTML + '<div class="user1"><img class="korisnik"><pre class="message">' + pitanje + '</pre></div>' + '<div class="user2"><img class="llama"><span class="loader"></span></div>';
+    var user2 = document.querySelectorAll(".user2");
+    var zadnjiOdgovor = user2[user2.length - 1];
+    zadnjiOdgovor.scrollIntoView({ behavior: "smooth" });
     document.getElementById("pitanje").value = "";
-    document.getElementById("poruka").innerHTML = "<h2>Vaš zahtjev se obrađuje</h2>";
-    document.getElementById("poruka").scrollIntoView({ behavior: "smooth" });
     $.ajax({
         url: '/pitanje',
         type: 'POST',
         data: { 'pitanje': pitanje },
         success: function(response) {
-            document.getElementById("poruka").innerHTML = "";
             var odgovor = response;
-            console.log(odgovor);
-            var chat = document.getElementById("chat");
-            chat.innerHTML = chat.innerHTML + '<div class="user2"><img class="llama"><p class="message">' + odgovor + '</p></div>';
-
             var user2 = document.querySelectorAll(".user2");
             var zadnjiOdgovor = user2[user2.length - 1];
+            zadnjiOdgovor.innerHTML = '<img class="llama"><pre class="message">' + odgovor + '</pre>';
             zadnjiOdgovor.scrollIntoView({ behavior: "smooth" });
         },
         error: function(error) {
             console.log(error);
         }
     });
+}
+
+function izbrisiRazgovor(){
+    $.ajax({
+        url: '/izbrisiChat',
+        type: 'POST',
+        success: function(response) {
+           // Opcionalno poruka o brisanju chata
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+    chat.innerHTML = "";
 }
 
 window.onload = function() {
@@ -38,7 +48,7 @@ window.onload = function() {
             type: 'POST',
             data: { 'model': filter.value },
             success: function(response) {
-                // poruka o promjeni modela.
+                // Opcionalno poruka o promjeni modela.
             },
             error: function(error) {
                 console.log(error);
